b691f546522a8f4fe39b192b75d43f76deee5d36
Dalton Eggleston <egglestondalton@yahoo.com>
2022-07-09 22:40:44 -0700

    partially done

--- ormQueries/products/product_crud.py
+++ ormQueries/products/product_crud.py
@@ -3,4 +3,23 @@ from .models import Product
 class ProductCrud:
     @classmethod
     def get_all_products(cls):
-        pass
+        return Product.objects.all()
+
+    @classmethod 
+    def find_by_model(cls, model):
+
+        products = Product.objects.all()
+
+
+        for product in products:
+            if product.model == model:
+                return product
+
+    @classmethod
+    def last_record(cls):
+        return Product.objects.last()
+
+    @classmethod
+    def by_rating(cls, rating):
+
+        return Product.objects.filter(rating = rating)
